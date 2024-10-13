e4927de1c00bb3caa1352930e2036f52f59e848a
PiyushBhandariShorthillsAI <piyush@shorthills.ai>
2024-10-13 18:01:50 +0530

    Added unit test cases for data extraction and data storage in the files

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from storage.sql_storage import SQLStorage
 
 if __name__ == "__main__":
     # Example usage with a PDF file
-    # file_loader = PDFLoader('/home/shtlp_0125/Desktop/Piyush_Assignment_4/sample_requirements_with_table_image.pdf')
-    file_loader = DOCXLoader('/home/shtlp_0125/Desktop/Piyush_Assignment_4/file-sample_100kB.docx')
+    file_loader = PDFLoader('/home/shtlp_0125/Desktop/Piyush_Assignment_4/sample_requirements_with_table_image.pdf')
+    # file_loader = DOCXLoader('/home/shtlp_0125/Desktop/Piyush_Assignment_4/file-sample_100kB.docx')
     # file_loader = PPTLoader('/home/shtlp_0125/Desktop/Piyush_Assignment_4/DALL·E 2024-10-13 13.09.57 - A simple PowerPoint presentation (PPTX) slide containing text, an embedded image, a hyperlink, and a data table. The slide has a title at the top, a s.pptx')
     extractor = DataExtractor(file_loader)
 
